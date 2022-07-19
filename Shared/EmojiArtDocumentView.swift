c7a968205f3322d825219d84f3311744363cda0c
Sasha <a.skinderev@gmail.com>
2022-07-19 05:49:51 +0300

    Fixed all errors on macOS app

--- Shared/EmojiArtDocumentView.swift
+++ Shared/EmojiArtDocumentView.swift
@@ -100,7 +100,7 @@ struct EmojiArtDocumentView: View {
     
     private func handlePickedBackgroundImage(_ image: UIImage?) {
         autozoom = true
-        if let imageData = image?.jpegData(compressionQuality: 1.0) {
+        if let imageData = image?.imageData {
             document.setBackground(.imageData(imageData), undoManager: undoManager)
         }
         backgroundPicker = nil
@@ -117,9 +117,9 @@ struct EmojiArtDocumentView: View {
     
     private func pasteBackground() {
         autozoom = true
-        if let imageData = UIPasteboard.general.image?.jpegData(compressionQuality: 1.0) {
+        if let imageData = Pasteboard.imageData {
             document.setBackground(.imageData(imageData), undoManager: undoManager)
-        } else if let url = UIPasteboard.general.url?.imageURL {
+        } else if let url = Pasteboard.imageURL {
             document.setBackground(.url(url), undoManager: undoManager)
         } else {
             alertToShow = IdentifiableAlert(
@@ -150,6 +150,7 @@ struct EmojiArtDocumentView: View {
             autozoom = true
             document.setBackground(.url(url.imageURL), undoManager: undoManager)
         }
+        #if os(iOS)
         if !found {
             found = providers.loadObjects(ofType: UIImage.self) { image in
                 if let data = image.jpegData(compressionQuality: 1.0) {
@@ -158,6 +159,7 @@ struct EmojiArtDocumentView: View {
                 }
             }
         }
+        #endif
         if !found {
             found = providers.loadObjects(ofType: String.self) { string in
                 if let emoji = string.first, emoji.isEmoji {
